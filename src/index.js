0f5f84a9a255356eea369f9de7fab7f2f4648632
Nguyen <cnguyen64@dxc.com>
2019-05-14 12:50:58 +0700

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+/*  eslint-disable no-unused-vars */
+
 import ReactDOM from 'react-dom';
+import React from 'react';
 
 const title = 'My Minimal React Webpack Babel Setup';
 
 ReactDOM.render(
   <div>{title}</div>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
-
 module.hot.accept();
